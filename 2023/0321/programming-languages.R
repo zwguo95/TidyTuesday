8d3e60efc4672886042ab9c5400794b7ad2f4b3f
zwguo95 <zwguo@uw.edu>
2023-03-26 22:30:20 -0700

    fix a typo

--- 2023/0321/programming-languages.R
+++ 2023/0321/programming-languages.R
@@ -18,7 +18,6 @@ languages <- languages %>%
     appeared >= 2000 & appeared < 2010 ~ 4
   ))
 
-background <- image_read("computer-desktop-icon.png")
 images <- c("icons/java.png",
             "icons/python.png",
             "icons/clojure.png",
@@ -50,23 +49,23 @@ tokens <- languages %>%
   mutate(yaxis = rep(c(3,1,4,2), each = 4)) %>%
   arrange(desc(yaxis))
 
-icon_data <- data.frame(ximage = rep(0, 4),
-                        xlabel = rep(0.3, 4),
+icon_data <- data.frame(ximage = rep(0.3, 4),
+                        xlabel = rep(0.5, 4),
                         y = c(4,3,2,1),
                         labels = c("//", "#", ";", "--"),
                         image = images)
 
 ggplot() +
   geom_tile(data = tokens, aes(y = yaxis, x = year_range, fill = num_language), color = "black",
-            height = 0.4, width = 1) +
-  scale_fill_gradient(low = "grey70", high = "grey10") +
+            height = 0.5, width = 0.5) +
+  scale_fill_gradient(low = "grey70", high = "grey20") +
   geom_text(data = tokens, aes(y = yaxis, x = year_range, label = num_language), color = "white", size = 15) +
-  geom_text(data = icon_data, aes(x = xlabel, y = y, label = labels), vjust = 1, color = "white", size = 20) +
+  geom_text(data = icon_data, aes(x = xlabel, y = y, label = labels), vjust = 1, color = "white", size = 25) +
   geom_image(data = icon_data, aes(x = ximage, y = y, image = images), asp=1.5, size=0.05) +
-  annotate(geom = "text", label = "1970s", x = 1, y = 0.6, color = "white", size = 16, family = "Roboto") +
-  annotate(geom = "text", label = "1980s", x = 2, y = 0.6, color =  "white", size = 16, family = "Roboto") +
-  annotate(geom = "text", label = "1990s", x = 3, y = 0.6, color = "white", size = 16, family = "Roboto") +
-  annotate(geom = "text", label = "2000s", x = 4, y = 0.6, color = "white", size = 16, family = "Roboto") +
+  annotate(geom = "text", label = "1970s", x = 1, y = 0.6, color = "white", size = 15, family = "Roboto") +
+  annotate(geom = "text", label = "1980s", x = 2, y = 0.6, color =  "white", size = 15, family = "Roboto") +
+  annotate(geom = "text", label = "1990s", x = 3, y = 0.6, color = "white", size = 15, family = "Roboto") +
+  annotate(geom = "text", label = "2000s", x = 4, y = 0.6, color = "white", size = 15, family = "Roboto") +
   
   labs(x = "", y = "", title = "Number of Programming Languages that Use Line Comment Tokens", 
        subtitle = "Among the 4,303 programming languages listed in the Programming Language DataBase, the most commonly used line comment tokens in descending order are //, #, ;, and --. These tokens are used in several popular programming languages including Java, Python, Clojure, and SQL.",
@@ -78,21 +77,21 @@ ggplot() +
         plot.margin = margin(l=15, r=15),
         plot.title = element_text(colour = "#66FF00",
                                   face = "bold",
-                                  size = 70,
+                                  size = 90,
                                   hjust = 0.5,
-                                  margin = margin(t=10)),
+                                  margin = margin(t=15)),
         plot.subtitle = element_textbox_simple(
-          lineheight = 0.3,
+          lineheight = 0.2,
           colour = "#66FF00",
           hjust = 0,
-          size = 50,
-          margin = margin(t = 10, l=20, r=20, b = 10)
+          size = 70,
+          margin = margin(t = 10, l=20, r=20, b = 20)
         ),
         plot.caption = element_textbox_simple(
           lineheight = 0.4,
           colour = "#66FF00",
-          size = 40,
-          margin = margin(t = 5, l=20, b=10)
+          size = 50,
+          margin = margin(t = 10, l=20, b=15)
         ))
 
 library(ggview)
